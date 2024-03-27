162747d6daa83c8ff4a7a2e7dda47b75595a5828
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-03-27 15:41:26 +0600

    Crate an Object

---
+++ JS_Object/main.js
@@ -0,0 +1,21 @@
+/*
+
+An Object is a unique entry that contains properties like:
+
+  * Variable type properties
+  * Method Properties
+  * Object Properties
+  * Array Properties
+  
+  There are 3 ways to create Objects
+
+
+*/
+
+let person = {
+    name: "Ostad",
+    age: 24,
+    city: 'Dhaka'
+}
+
+console.log(person);
