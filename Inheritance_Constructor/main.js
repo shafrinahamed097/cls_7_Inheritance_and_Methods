45dabca69b05ec05c0c56cd173a43d70e2158811
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-03-29 14:42:04 +0600

    Constructor inside both parent & child class pass parameters

--- Inheritance_Constructor/main.js
+++ Inheritance_Constructor/main.js
@@ -29,5 +29,25 @@ class Son1 extends Father1 {
     }
 }
 
-let SonObj1 = new Son1(20, 30);
+// let SonObj1 = new Son1(20, 30);
+
+
+// Constructor inside both parent & child class pass parameters
+
+
+class Father2 {
+    constructor(a, b) {
+        console.log("I'm Father Constructor = " + (a + b));
+    }
+}
+
+class Son2 extends Father2 {
+    constructor(a, b) {
+        super();
+        console.log("I'm Son Constructor = " + (a + b));
+    }
+
+}
+
+let SonObj2 = new Son2(30, 40);
 
