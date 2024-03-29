411542f60eaf1ec5dda03f90276686495adc3f89
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-03-29 14:28:46 +0600

    Constructor inside only child class

--- Inheritance_Constructor/main.js
+++ Inheritance_Constructor/main.js
@@ -12,5 +12,22 @@ class Son extends Father {
 
 }
 
-let SonObj = new Son(20, 30);
-let FatherObj = new Father(40, 30);
+// let SonObj = new Son(20, 30);
+// let FatherObj = new Father(40, 30);
+
+
+// Constructor inside only Child Class
+
+class Father1 {
+
+}
+
+class Son1 extends Father1 {
+    constructor(a, b) {
+        super();
+        console.log("I'm Son Constructor = " + (a + b));
+    }
+}
+
+let SonObj1 = new Son1(20, 30);
+
